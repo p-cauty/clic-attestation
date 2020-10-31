332494fc3e256b26e7f713fcd91f58b54c1a0f4b
jeremypgn <pingeonjeremy@gmail.com>
2020-10-31 22:25:56 +0100

    Adding analytic tracker

--- app/views/mainView.php
+++ app/views/mainView.php
@@ -12,6 +12,22 @@
         <link rel="apple-touch-icon" type="image/x-icon" href="<?= ASSETS ?>/img/favicon.png" />
         <script data-search-pseudo-elements defer src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/js/all.min.js" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/feather-icons/4.24.1/feather.min.js" crossorigin="anonymous"></script>
+       
+        <!-- Matomo -->
+        <script type="text/javascript">
+          var _paq = window._paq = window._paq || [];
+          /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+          _paq.push(['trackPageView']);
+          _paq.push(['enableLinkTracking']);
+          (function() {
+            var u="//analytics.justauth.me/";
+            _paq.push(['setTrackerUrl', u+'matomo.php']);
+            _paq.push(['setSiteId', '3']);
+            var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+            g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+          })();
+        </script>
+        <!-- End Matomo Code -->
     </head>
     <body>
         <div id="layoutDefault">
